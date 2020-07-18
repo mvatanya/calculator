69d3b656fe8168ebaa8d505079730500b815bf8c
Marisa Vatanya <mvatanya@gmail.com>
2020-07-18 11:34:53 -0700

    added handle click numbers and handle click operators, start handle result

--- calculator.js
+++ calculator.js
@@ -1,22 +1,21 @@
 let operations = {
   '+': function(a,b) {return a+b},
   '-': function(a,b) {return a-b},
-  '*': function(a,b) {return a*b},
-  '/': function(a,b) {return a/b}
+  '×': function(a,b) {return a*b},
+  '÷': function(a,b) {return a/b}
 }
-
+let operators = Object.keys(operations)
 let input = document.getElementById('input');
-let numbers = document.querySelectorAll('.numbers div');
-let operators = document.querySelectorAll('.operators-container div')
+let numbersDiv = document.querySelectorAll('.numbers div');
+let operatorsDiv = document.querySelectorAll('.operators-container div')
 let showResult = false
 let lastChar
 
 //To handle when click numbers
-numbers.forEach(n => {
+numbersDiv.forEach(n => {
   n.addEventListener('click', function(e){
     //case 1 if showResult is false, keep adding number on input
     if(!showResult){
-      console.log(e)
       input.innerText += e.target.innerText
     }
 
@@ -30,14 +29,44 @@ numbers.forEach(n => {
 
 
 //To handle when click on operators
-//case 1 if length on display is 0; ignore the operator
+operatorsDiv.forEach(o => {
+  o.addEventListener('click', function(e){
+    lastChar = input.innerText[input.innerText.length-1]
+    //case 1 if length on display is 0; ignore the operator
+    if(input.innerText.length === 0){
+      input.innerText = '';
+    //case 2 if length on display is not 0 but last char is operator, replace that last char operator to new one
+    } else if (input.innerText.length !== 0 && operators.indexOf(lastChar) !== -1 ){
+      let newString = input.innerText.slice(0, input.innerText.length-1)
+      input.innerText = newString + e.target.innerText
+    //case 3 add operator to display
+    } else { 
+      input.innerText += e.target.innerText
+    }
+  })
+})
 
-//case 2 if length on display is not 0 but last char is operator, replace that last char operator to new one
-
-//else add operator to display
 
 //To handle clear
-let clear = document.getElementById('clear')
-clear.addEventListener('click', function(){
+let clearDiv = document.getElementById('clear')
+clearDiv.addEventListener('click', function(){
   input.innerText = ''
+})
+
+//To handle result
+let resultDiv = document.getElementById('equals')
+resultDiv.addEventListener('click', function(){
+  let currentText = input.innerText
+  //Check if last text is one of the operations, remove it
+  if (operators.indexOf(currentText[currentText.length-1])!==-1){
+    console.log('in here', operators.indexOf(currentText[currentText.length-1]))
+    let newString = input.innerText.slice(0, input.innerText.length-1)
+    currentText = newString
+  }
+  //Seperate numbers and operations into different arrays
+  console.log(currentText)
+  let numbersArray = currentText.split(/\+|\-|\×|\÷/g);
+  let operatorsArray = currentText.replace(/[0-9]|\./g, "").split("");
+  console.log(numbersArray)
+  console.log(operatorsArray)
 })
